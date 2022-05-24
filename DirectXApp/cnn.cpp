91b92f9845f2d391d38a2c27a2b5d382d1a9f52d
YaruDu <yaru.du@intel.com>
2022-05-24 12:00:07 +0800

    get output from CPU memory

--- DirectXApp/cnn.cpp
+++ DirectXApp/cnn.cpp
@@ -51,6 +51,25 @@ void Cnn::Init(const std::string& model_path, ID3D11Device*& d3d_device, cv::Mat
     ov::Tensor input_tensor = ov::Tensor(input_type, input_shape, input_data.data);
     infer_request.set_input_tensor(input_tensor);
     infer_request.infer();
+    for (auto&& output : compiled_model.outputs()) {
+        const std::string name = output.get_names().empty() ? "NONE" : output.get_any_name();
+        if (name == "NONE")
+        {
+            continue;
+        }
+
+        const ov::Tensor& output_tensor = infer_request.get_tensor(name);
+
+        auto data_size = output_tensor.get_size();
+        auto data = output_tensor.data<int64_t>();
+        std::cout << name.substr(0, name.find(":")) << ": ";
+        for (size_t i = 0; i < data_size; i++) {
+            std::cout << data[i] << " ";
+            break;
+        }
+        std::cout << std::endl;
+    }
+    is_initialized_ = true;
 }
 
 void Cnn::Init(const std::string &model_path,  ID3D11Device*& d3d_device, ID3D11Buffer* input_buffer, ID3D11Buffer* output_buffer, const cv::Size &new_input_resolution) {
@@ -132,10 +151,74 @@ void Cnn::Init(const std::string &model_path,  ID3D11Device*& d3d_device, ID3D11
     ov::Shape output_shape = { 1, 720,1280, 3 };
     auto shared_in_blob = gpu_context.create_tensor(ov::element::u8, input_shape, input_buffer);
     //auto shared_in_blob1 = gpu_context.create_tensor(ov::element::u8, input_shape, input_surface);
-    auto shared_output_blob = gpu_context.create_tensor(ov::element::f32, output_shape, output_buffer);
+   // auto shared_output_blob = gpu_context.create_tensor(ov::element::f32, output_shape, output_buffer);
     infer_request.set_input_tensor(shared_in_blob);
-    infer_request.set_output_tensor(shared_output_blob);
+   // infer_request.set_output_tensor(shared_output_blob);
     infer_request.infer();
+
+    int idx = 0;
+    for (auto&& output : exec_net_shared.outputs()) {
+
+        //const std::string name = output.get_names().empty() ? "NONE" : output.get_any_name();
+        //if (name == "NONE")
+        //{
+        //    continue;
+        //}
+        const ov::Tensor& output_tensor =  infer_request.get_output_tensor(idx);
+        idx++;
+
+        //const ov::Tensor& output_tensor = infer_request.get_tensor(name);
+
+        auto data_size = output_tensor.get_size();
+       
+       /* std::cout << name.substr(0, name.find(":")) << ": ";*/
+        std::cout << std::to_string(idx) << ":";
+        int i = 0;
+        if (idx == 1) {
+
+            std::cout << std::endl;
+            continue;
+        }
+
+        auto data = output_tensor.data<uint8_t>();
+        for (size_t i = 0; i < data_size; i++) {
+            std::cout << (int)data[i] << " ";
+            i++;
+            if (i > 614400) break;
+        }
+        std::cout << std::endl;
+        /*float* data_ptr = reinterpret_cast<float*>(output_tensor.data());
+        std::vector<float> output_data(data_ptr, data_ptr + data_size);
+
+        for (int i = 0; i < output_data.size(); i++) {
+            std::cout << output_data[i] << " ";
+            i++;
+            if (i > 300) break;
+
+        }*/
+
+        std::cout << std::endl;
+
+        //int rows = 720;
+        //int cols = 1280;
+        //cv::Mat outputImage;
+        //if (data_size == rows * cols * 3) // check that the rows and cols match the size of your vector
+        //{
+        //    //copy vector to mat
+        //    cv::Mat channelR(rows, cols, CV_32FC1, data);
+        //    cv::Mat channelG(rows, cols, CV_32FC1, data + cols * rows);
+        //    cv::Mat channelB(rows, cols, CV_32FC1, data + 2 * cols * rows);
+        //    // RGB2BGR
+        //    std::vector<cv::Mat> channels{ channelB, channelG, channelR };
+
+        //    // Create the output matrix
+        //    merge(channels, outputImage);
+        //}
+        ////postprocessing
+        //// normolize  (-1,1) to (0,255)
+        //cv::normalize(outputImage, outputImage, 0, 255, cv::NORM_MINMAX);
+        //cv::imwrite("styled.png", outputImage);
+    }
     is_initialized_ = true;
 }
 
