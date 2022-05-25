14bf2a1bdd98450cfebb82385e42fa9336bde37a
YaruDu <yaru.du@intel.com>
2022-05-25 18:15:18 +0800

    Big step: runnable for nv12 two planes

--- DirectXApp/cnn.cpp
+++ DirectXApp/cnn.cpp
@@ -64,18 +64,19 @@ void Cnn::Init(const std::string &model_path,  ID3D11Device*& d3d_device, ID3D11
 
     ppp.input().tensor().
         set_element_type(ov::element::u8).
-        set_color_format(ov::preprocess::ColorFormat::NV12_SINGLE_PLANE).
-        set_layout("NHWC").
-        set_spatial_static_shape(480, 640);
-        //set_memory_type(ov::intel_gpu::memory_type::surface);
+        set_color_format(ov::preprocess::ColorFormat::NV12_TWO_PLANES, { "y", "uv" }).
+        //set_layout("NHWC").
+        set_spatial_static_shape(480, 640).
+        set_memory_type(ov::intel_gpu::memory_type::surface);
+
     // 3) Adding explicit preprocessing steps:
     // - convert u8 to f32
     // - convert layout to 'NCHW' (from 'NHWC' specified above at tensor layout)
     ppp.input().preprocess().
-        convert_element_type(ov::element::f32).
         convert_color(ov::preprocess::ColorFormat::BGR).
         resize(ov::preprocess::ResizeAlgorithm::RESIZE_LINEAR).
-        convert_layout("NCHW")
+        convert_element_type(ov::element::f32)
+        //convert_layout("NCHW")
         ; 
 
     ppp.input().model().set_layout("NCHW");
@@ -98,7 +99,7 @@ void Cnn::Init(const std::string &model_path,  ID3D11Device*& d3d_device, ID3D11
     ov::intel_gpu::ocl::D3DContext gpu_context(core, d3d_device);
 
     remote_context = &gpu_context;
-    auto exec_net_shared = core.compile_model(model, gpu_context); // change device to RemoteContext
+    auto exec_net_shared = core.compile_model(model, gpu_context, { { GPUConfigParams::KEY_GPU_NV12_TWO_INPUTS, PluginConfigParams::YES} }); // change device to RemoteContext
     ov::serialize(exec_net_shared.get_runtime_model(),"test_graph.xml");
 
 
@@ -108,11 +109,12 @@ void Cnn::Init(const std::string &model_path,  ID3D11Device*& d3d_device, ID3D11
     //infer_request_ = executable_network.CreateInferRequest();
     //// ---------------------------------------------------------------------------------------------------
 
-    ov::Shape input_shape = { 1, 720, 640 ,1};
+    ov::Shape input_shape = { 1, 480, 640 ,1};
     ov::Shape output_shape = { 1, 720,1280, 3 };
-    auto shared_in_blob = gpu_context.create_tensor(ov::element::u8, input_shape, input_surface);
+    auto shared_in_blob = gpu_context.create_tensor_nv12(input_shape[1],input_shape[2], input_surface);
     //auto shared_output_blob = gpu_context.create_tensor(ov::element::f32, output_shape, output_surface);
-    infer_request.set_input_tensor(shared_in_blob);
+    infer_request.set_tensor("inputImage/y",shared_in_blob.first);
+    infer_request.set_tensor("inputImage/uv",shared_in_blob.second);
     //infer_request.set_output_tensor(shared_output_blob);
     infer_request.infer();
     is_initialized_ = true;
